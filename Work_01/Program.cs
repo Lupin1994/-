c324fac76ec90007d5068a369e9bddb9e6172c5a
Alex <skorpion_94@list.ru>
2022-07-03 18:48:51 +0300

    Максимальное из трех чисел

--- Work_01/Program.cs
+++ Work_01/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Write first number: ");
-int F_num = int.Parse(Console.ReadLine()??"");
+int F_num = int.Parse(Console.ReadLine() ?? "");
 Console.WriteLine("Write second number: ");
-int S_num = int.Parse(Console.ReadLine()??"");
+int S_num = int.Parse(Console.ReadLine() ?? "");
 if (F_num > S_num)
 {
     Console.WriteLine($"max = {F_num} , min = {S_num}");
 }
-else 
-Console.WriteLine($"max = {S_num} , min = {F_num}");
+else
+    Console.WriteLine($"max = {S_num} , min = {F_num}");
---
+++ Work_02/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Write first number: ");
+int F_num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write second number: ");
+int S_num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write third number: ");
+int T_num = int.Parse(Console.ReadLine() ?? "");
+int max = F_num;
+if (max < S_num)
+{
+    max = S_num;
+}
+if (max < T_num)
+{
+    max = T_num;
+}
+Console.WriteLine($"max = {max}");
+
