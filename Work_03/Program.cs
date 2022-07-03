54c2c51b26f42022f3c4ca05c283a54825c7a439
Alex <skorpion_94@list.ru>
2022-07-03 18:51:48 +0300

    Четное ли число

---
+++ Work_03/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Write number:");
+int Number = int.Parse(Console.ReadLine() ?? "");
+if (Number % 2 == 0)
+{
+    Console.WriteLine("Number even");
+}
+else
+{
+    Console.WriteLine("Number odd");
+}
