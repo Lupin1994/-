b6f054cd40839c4105663754f87d9fd11021470f
Alex <skorpion_94@list.ru>
2022-07-03 18:14:36 +0300

    Start

--- Work_01/Program.cs
+++ Work_01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Write first number: ");
+int F_num = int.Parse(Console.ReadLine()??"");
+Console.WriteLine("Write second number: ");
+int S_num = int.Parse(Console.ReadLine()??"");
+if (F_num > S_num)
+{
+    Console.WriteLine($"max = {F_num} , min = {S_num}");
+}
+else 
+Console.WriteLine($"max = {S_num} , min = {F_num}");
