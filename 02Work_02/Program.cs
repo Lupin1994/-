70b059a716fae48cfb95a8d74ef0945c5dafdb34
Alex <skorpion_94@list.ru>
2022-07-03 19:31:44 +0300

    Найти 3 число или сообщить что его нет

---
+++ 02Work_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введи число: ");
+int Num = int.Parse(Console.ReadLine() ?? "");
+int digit_3 = 0;
+if(Num<99)
+{
+    Console.WriteLine("In your number is not thrird digit");
+}
+if(Num<1000)
+{
+    digit_3 = Num%10;
+    Console.WriteLine($"This is third digit {digit_3}");
+}
+else
+{
+    digit_3 = (Num%1000)/100;
+    Console.WriteLine("This is third digit");
+    Console.WriteLine(digit_3);
+}
