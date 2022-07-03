ebcec64295c74b3d6a0f3f6334aa100c7f0856ba
Alex <skorpion_94@list.ru>
2022-07-03 20:03:08 +0300

    3D

---
+++ 03Work_02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+double dis_AB = 0;
+
+Console.WriteLine("Write coordinete (.) A:");
+Console.WriteLine("Write x = ");
+int x_A = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write y = ");
+int y_A = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write z = ");
+int z_A = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"(.) A = ({x_A} , {y_A} , {z_A})");
+
+Console.WriteLine("Write coordinete (.) B:");
+Console.WriteLine("Write x = ");
+int x_B = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write y = ");
+int y_B = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Write z = ");
+int z_B = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine($"(.) B = ({x_B} , {y_B} , {z_B})");
+
+Console.WriteLine("Calculate the distance between (.) A and (.) B");
+dis_AB = Convert.ToInt32(Math.Sqrt((x_B - x_A) + (y_B - y_A) + (z_B - z_A)));
+Console.WriteLine($"AB = {dis_AB}");
